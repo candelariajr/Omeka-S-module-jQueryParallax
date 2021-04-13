757f08391e8d0a5e4a7619065da24bd249bf0175
Jonathan Candelaria <candelariajr@appstate.edu>
2021-04-13 12:04:31 -0400

    Initial Commit

---
+++ Module.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Copyright Jonathan Candelaria 2021
+ * 
+ * This software is available under the open and fair use policy.
+ *
+ * Parallax.js © 2017 PixelCog Inc. released under MIT license
+ */
+
+namespace jQueryParallax;
+
+use Omeka\Module\AbstractModule;
+
+class Module extends AbstractModule
+{
+    public function getConfig()
+    {
+        return [];
+    }
+}
